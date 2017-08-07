56e8d5faafa88cbcae651a8d66329c4330137ec5
Bartłomiej Wiaduch <tukan2can@gmail.com>
2017-08-07 14:28:37 +0200

    feature: design

--- website/javascripts/main.js
+++ website/javascripts/main.js
@@ -1,1 +1,34 @@
+
 console.log("Hello, I'm Kulfon");
+
+(function () {
+
+  'use strict';
+
+  let accordions, i;
+
+  function makeAccordion(accordion) {
+    let targets, currentTarget;
+    targets = accordion.querySelectorAll('.menu-list >  .accordion-toggle');
+
+    function expand() {
+      if (currentTarget) {
+        currentTarget.classList.remove('d-none');
+      }
+      currentTarget = this.nextElementSibling;
+      currentTarget.classList.add('d-block');
+    }
+
+    for (i = 0; i < targets.length; i += 1) {
+      targets[i].addEventListener('click', expand, false);
+    }
+
+  }
+
+  accordions = document.querySelectorAll('.menu-list');
+
+  for (i = 0; i < accordions.length; i += 1) {
+    makeAccordion(accordions[i]);
+  }
+
+}());
